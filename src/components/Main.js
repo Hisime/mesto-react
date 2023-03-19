576fa05d2f75ac531aa3406a33b2c5e1d7abe9ed
Hisime <rihisime@gmail.com>
2023-03-20 00:14:34 +0400

    Подключение API

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,29 @@
-import profileImage from '../images/profile.jpg';
+import React from "react";
+import api from "../utils/api";
 
 function Main(props) {
+    const [userName, setUserName] = React.useState();
+    const [userDescription, setUserDescription] = React.useState();
+    const [userAvatar, setUserAvatar] = React.useState();
+    React.useEffect(() => {
+        api.getUser().then((res) => {
+            setUserName(res.name);
+            setUserDescription(res.about);
+            setUserAvatar(res.avatar);
+        })
+    }, []);
     return (
         <main>
             <section className="profile">
                 <div className="profile__avatar">
-                    <img className="profile__image" src={profileImage} alt="Аватар"/>
+                    <img className="profile__image" src={userAvatar} alt="Аватар"/>
                     <button className="profile__edit-avatar" onClick={props.onEditAvatar}></button>
                 </div>
 
                 <div className="profile__info">
-                    <h1 className="profile__name"></h1>
+                    <h1 className="profile__name">{userName}</h1>
                     <button className="profile__edit" type="button" aria-label="Редактировать" onClick={props.onEditProfile}></button>
-                    <p className="profile__description"></p>
+                    <p className="profile__description">{userDescription}</p>
                 </div>
                 <button className="profile__add" type="button" aria-label="Добавить" onClick={props.onAddPlace}></button>
             </section>
